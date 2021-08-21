8d547e9e4ae096df348f08b3bb8b2c30585e43ab
Erik <erikadarisman@gmail.com>
2021-08-21 17:39:44 +0700

    add link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 // Step 1: Import React
+import { Link } from 'gatsby'
 import * as React from 'react'
 // Step 2: Define your component
 const AboutPage = () => {
@@ -6,6 +7,7 @@ const AboutPage = () => {
     <main>
       <title>About Me</title>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
     </main>
   )
